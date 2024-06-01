1df75f51bcd8b4246f83a8495cbb0a676fdb3ffb
minh trung <trung.nm226004@sis.hust.edu.vn>
2024-06-01 21:36:07 +0700

    Diagrams Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module view.interactivesimulation {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.naming;
+    requires javafx.graphics;
 
     opens view.interactivesimulation to javafx.fxml;
     opens controller to javafx.fxml;
